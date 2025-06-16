4f3d253e5db11a63f10697cedce8847bdd09ca02
yuvraaajsingh <yuvraj2802singh@gmail.com>
2025-06-16 17:00:54 +0530

    Completed assignment

---
+++ AccioJob/React_Practice_Question/01_Single_component/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div id='main'>
+      <p>"Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
+}
+
+export default App
